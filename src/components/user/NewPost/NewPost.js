a2afeaf07352835088c0c0c49d4acdbea1015289
MinhCong <dmcongitus@gmail.com>
2019-04-22 01:28:40 +0700

    new post

--- src/components/user/NewPost/NewPost.js
+++ src/components/user/NewPost/NewPost.js
@@ -1,18 +1,78 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { Container, Row, Col, Alert } from "reactstrap";
-
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  InputGroupAddon,
+  InputGroup
+} from "reactstrap";
 import "./NewPost.css";
 
 const NewPost = props => (
-    <div id="status-overlay" style="display: none">
-    
-    <form id="highlight-textarea" class="form">
-    <textarea onclick="highlight();" name="postText" cols="10" rows="3" placeholder="What's going on?"></textarea>
-    <input type="button" value="Post"/>
-    </form>
+  <div className="NewPostBox col">
+    <Row>
+      <Alert style={{width: '100%'}} color="success">Tạo Bài Viết</Alert>
+    </Row>
 
-    </div>
+    <Row>
+      <Col xs="9">
+        <Row >
+        <Col xs="2">
+
+          <img
+            className="img-user-postCard rounded-circle"
+            src="https://photo-2-baomoi.zadn.vn/w1000_r1/2018_08_06_181_27170707/a5250170ac3745691c26.jpg"
+          />
+        </Col>
+        <Col xs="10">
+
+          <Form >
+            <FormGroup >
+              <Input
+                className="TextBox-input"
+                type="textarea"
+                name="text"
+                id="exampleText"
+                placeholder="Hãy nói gì về bức ảnh này."
+                style={{height: '100px', width:'100%'}}
+              />
+            </FormGroup>
+          </Form>
+        </Col>
+        </Row>
+      </Col>
+      <Col xs="3">
+        <img
+          id="blah"
+          className="imagePost-input"
+          src="http://icons.iconarchive.com/icons/designbolts/free-multimedia/1024/Photo-icon.png"
+        />
+      </Col>
+    </Row>
+    <Row>
+   
+        <div className="checkInPost" style={{width: '100%'}}>
+          <InputGroup >
+            <InputGroupAddon addonType="prepend" >
+              <Button color="primary">Tại</Button>
+            </InputGroupAddon>
+            <Input />
+          </InputGroup>
+        </div>
+   
+    </Row>
+    <Row class="float-right">
+      <Col sm={{ size: "auto", offset: 9 }}>
+        <div className="btnPost">
+          <Button color="success">Đăng tải</Button>
+        </div>
+      </Col>
+    </Row>
+  </div>
 );
 
 export default NewPost;
--- src/containers/user/HomePage/HomePage.js
+++ src/containers/user/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Post from "../../../components/user/Post/PostList/Post";
+import NewPost from '../../../components/user/NewPost/NewPost'
 import "./HomePage.css";
 
 class HomePage extends Component {
@@ -8,6 +9,7 @@ class HomePage extends Component {
     return (
       <div className="widget-sidebar">
         <h2 className="title-widget-sidebar"># NEWS</h2>
+        <NewPost></NewPost>
         <Post></Post>
       </div>
     );
