120ff439bf9e776eb9868c97817394c332af6612
ntncsebku <ntncsebku@gmail.com>
2019-04-24 00:12:59 +0700

    Add create post

--- src/components/user/NewPost/NewPost.js
+++ src/components/user/NewPost/NewPost.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import React, { Component, createRef } from 'react';import { NavLink, Link } from "react-router-dom";
 import { Container, Row, Col, Alert } from "reactstrap";
 import {
   Button,
@@ -12,82 +11,135 @@ import {
 } from "reactstrap";
 import "./NewPost.css";
 
-const NewPost = props => (
-  <div className="NewPostBox col">
-    <Row>
+import * as postServices from '../../../services/post.service';
 
-      <Alert style={{width: '100%'}} color="success">
-      <Row>
-      <Col xs="9">
-      Tạo Bài Viết
+const initialState = {
+    image: undefined,
+    description: '',
+    address: '',
+    type: "Hoạt động cá nhân",
+};
+
+class NewPost extends Component {
+    constructor(props) {
+        super(props);
+
+        this.inputImage = createRef();
+    }
+
+    state = initialState
+
+    handleImageChange = (e) => {
+        e.persist();
+        this.setState({ image: e.target.files[0] });
+    }
+
+    onTypeChange = (e) => {
+        this.setState({ type: e.target.value });
+    }
+
+    onFieldChanged = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit = (e) => {
+        postServices.createPost(this.state).then(() => {
+            this.setState({ ...initialState });
+            alert('Tạo bài viết thành công');
+        });
+    }
+
+    render() {
+        return (
+            <div className="NewPostBox col">
+                <Row>
+
+                    <Alert style={{ width: '100%' }} color="success">
+                        <Row style={{ display: 'flex', alignItems: 'center' }}>
+                            <Col xs="9">
+                                Tạo Bài Viết
       </Col>
-      <Col xs="3">
-      a
-      </Col>
-      </Row>
+                            <Col xs="3">
+
+                                <FormGroup style={{ marginBottom: 0 }}>
+                                    <Input type="select" name="select" id="exampleSelect" onChange={this.onTypeChange} value={this.state.type}>
+                                        <option value="Hoạt động cá nhân">Cá nhân</option>
+                                        <option value="Địa điểm">Địa điểm</option>
+                                    </Input>
+                                </FormGroup>
+                            </Col>
+                        </Row>
       
-      </Alert>
-    </Row>
+                    </Alert>
+                </Row>
 
-    <Row>
-      <Col xs="8">
-        <Row >
-        <Col xs="2">
+                <Row>
+                    <Col xs="8">
+                        <Row >
+                            <Col xs="2">
 
-          <img
-            className="img-user-postCard rounded-circle"
-            src="https://photo-2-baomoi.zadn.vn/w1000_r1/2018_08_06_181_27170707/a5250170ac3745691c26.jpg"
-          />
-        </Col>
-        <Col xs="10">
+                                <img
+                                    className="img-user-postCard rounded-circle"
+                                    src="https://photo-2-baomoi.zadn.vn/w1000_r1/2018_08_06_181_27170707/a5250170ac3745691c26.jpg"
+                                />
+                            </Col>
+                            <Col xs="10">
 
-          <Form >
-            <FormGroup >
-              <Input
-                className="TextBox-input"
-                type="textarea"
-                name="text"
-                id="exampleText"
-                placeholder="Hãy nói gì về bức ảnh này."
-                style={{height: '100px', width:'100%'}}
-              />
-            </FormGroup>
-          </Form>
-        </Col>
-        </Row>
-      </Col>
-      <Col xs="4">
-      <div className="Newpost-img">
-        <img
-          src="https://lh3.googleusercontent.com/-ojLI116-Mxk/WM1ZIwdnuwI/AAAAAAAADeo/4K6VpwIPSfgsmlXJB5o0N8scuI3iW4OpwCJoC/w424-h318-n-rw/thumbnail6.jpg"
-         
-        />
-      </div>
-      </Col>
-    </Row>
+                                <Form >
+                                    <FormGroup >
+                                        <Input
+                                            className="TextBox-input"
+                                            type="textarea"
+                                            name="description"
+                                            id="exampleText"
+                                            placeholder="Hãy nói gì về bức ảnh này."
+                                            style={{ height: '100px', width: '100%' }}
+                                            value={this.state.description}
+                                            onChange={this.onFieldChanged}
+                                        />
+                                    </FormGroup>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xs="4">
+                        <div className="Newpost-img">
+                            <img onClick={() => console.log(this.inputImage.current.click())}
+                                src={this.state.image ? URL.createObjectURL(this.state.image) : "https://lh3.googleusercontent.com/-ojLI116-Mxk/WM1ZIwdnuwI/AAAAAAAADeo/4K6VpwIPSfgsmlXJB5o0N8scuI3iW4OpwCJoC/w424-h318-n-rw/thumbnail6.jpg"}
+         alt="fucku"
+                            />
+
+                            <input type="file" style={{ display: 'none' }} ref={this.inputImage} onClick={e => e.target.value = null} onChange={this.handleImageChange}/>
+                        </div>
+                    </Col>
+                </Row>
     
-    <Row>
+                <Row>
    
-        <div className="checkInPost" style={{width: '100%'}}>
-          <InputGroup >
-            <InputGroupAddon addonType="prepend">
+                    <div className="checkInPost" style={{ width: '100%' }}>
+                        <InputGroup >
+                            <InputGroupAddon addonType="prepend">
           
-              <Button outline color="success">Tại</Button>
-            </InputGroupAddon>
-            <Input />
-          </InputGroup>
-        </div>
+                                <Button outline color="success">Tại</Button>
+                            </InputGroupAddon>
+                            <Input name="address" value={this.state.address} onChange={this.onFieldChanged}/>
+                        </InputGroup>
+                    </div>
    
-    </Row>
+                </Row>
     
-    <Row class="float-right">
-      <Col sm={{ size: "auto", offset: 9 }}>
-        <div className="btnPost">
-          <Button color="success">Đăng tải</Button>
-        </div>
-      </Col>
-    </Row>
-  </div>
-);
+                <Row class="float-right">
+                    <Col sm={{ size: "auto", offset: 9 }}>
+                        <div className="btnPost">
+                            <Button color="success" onClick={this.onSubmit}>Đăng tải</Button>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        );
+    
+    
+    }
+}
 
 export default NewPost;
