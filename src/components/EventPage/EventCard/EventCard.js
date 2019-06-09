b143350811dd2e6ae2ebf88308d9c4819c525af7
Cong <dmcongitus@gmail.com>
2019-06-09 10:41:51 +0700

    delete image + add image test

--- src/components/EventPage/EventCard/EventCard.js
+++ src/components/EventPage/EventCard/EventCard.js
@@ -15,27 +15,8 @@ import {
     CarouselIndicators,
     CarouselCaption
 } from "reactstrap";
-import img1 from "../../../images/1.jpg";
-import img2 from "../../../images/2.jpg";
-import img3 from "../../../images/3.jpg";
+
 import "./EventCard.css";
-const items = [
-    {
-        src: img1,
-        altText: "Slide 1",
-        caption: "Slide 1"
-    },
-    {
-        src: img2,
-        altText: "Slide 2",
-        caption: "Slide 2"
-    },
-    {
-        src: img3,
-        altText: "Slide 3",
-        caption: "Slide 3"
-    }
-];
 
 
 class EventCard extends React.Component {
@@ -115,7 +96,7 @@ class EventCard extends React.Component {
     render() {
         const { activeIndex } = this.state;
 
-        const slides = items.map(item => {
+        const slides = this.state.items.map(item => {
         return (
             <CarouselItem
                 onExiting={this.onExiting}
