01f1f88573d9f09924b94eb303d8ba650fff7c52
1512215 <huyhungpq64@gmail.com>
2019-06-11 19:36:07 +0700

    no message

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Button } from 'reactstrap';
+import * as authActions from './actions/auth.action';
+
+import Router from './router';
+
+import './App.css';
+
+class App extends Component {
+    componentDidMount = () => {
+        this.props.getUser();
+    }
+
+    render() {
+        if (this.props.isLoading) {
+            return ( <div>Loading</div>);
+        }
+
+        return (
+            <Router isAuthenticated={this.props.isAuthenticated} permission={this.props.user && this.props.user.permission} username={this.props.user && this.props.user.username} />
+        );
+    }
+}
+
+const mapStateToProps = ({ auth: { isAuthenticated, user }, ui: {isLoading}}) => ({ isAuthenticated, user, isLoading });
+
+const mapDispatchToProps = dispatch => ({
+    getUser: () => dispatch(authActions.getUser())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
