6e2b4d15fcf862243f8335a6937782d29b9460e0
Cong <dmcongitus@gmail.com>
2019-06-18 23:29:25 +0700

    loading

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Button } from 'reactstrap';
-import * as authActions from './actions/auth.action';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Button } from "reactstrap";
+import * as authActions from "./actions/auth.action";
 
-import Router from './router';
-
-import './App.css';
+import Router from "./router";
+import { Loading } from "element-react";
+import "./App.css";
 
 class App extends Component {
-    componentDidMount = () => {
-        this.props.getUser();
+  componentDidMount = () => {
+    this.props.getUser();
+  };
+
+  render() {
+    if (this.props.isLoading) {
+      return (
+        <div
+          style={{
+            background: "rgba(152, 251, 152, 0.44)",
+            height: "100vh",
+            fontSize: "4rem"
+          }}
+          className="item-mid item-center"
+        >
+          Đang tải...
+        </div>
+      );
     }
 
-    render() {
-        if (this.props.isLoading) {
-            return ( <div>Loading</div>);
-        }
-
-        return (
-            <Router isAuthenticated={this.props.isAuthenticated} permission={this.props.user && this.props.user.permission} username={this.props.user && this.props.user.username} />
-        );
-    }
+    return (
+      <Router
+        isAuthenticated={this.props.isAuthenticated}
+        permission={this.props.user && this.props.user.permission}
+        username={this.props.user && this.props.user.username}
+      />
+    );
+  }
 }
 
-const mapStateToProps = ({ auth: { isAuthenticated, user }, ui: {isLoading}}) => ({ isAuthenticated, user, isLoading });
+const mapStateToProps = ({
+  auth: { isAuthenticated, user },
+  ui: { isLoading }
+}) => ({ isAuthenticated, user, isLoading });
 
 const mapDispatchToProps = dispatch => ({
-    getUser: () => dispatch(authActions.getUser())
-})
+  getUser: () => dispatch(authActions.getUser())
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
